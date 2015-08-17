65602385be84cc9b1b5467699a5d6b23fb9b9b90
Cruz Julian Bishop <cruzjbishop@gmail.com>
2015-08-18 08:04:10 +1000

    Get the skeleton of the new OutgoingPacket trait up and running
    Refers to #7
    
    This has not yet been tested or implemented

--- src/io/network/packet/outgoing/mod.rs
+++ src/io/network/packet/outgoing/mod.rs
@@ -1,9 +1,55 @@
 //! The module containing all outgoing packet implementations
-//!
-//! Enjoy
+
+use super::super::deque_buffer::DequeBuffer;
+use super::super::game_connection::GameConnection; //It's super super super effective!
+
+use std::io::Write;
 
 //pub mod handshake; No handshake packets (yet)!
 pub mod status;
+
+/// A trait for outgoing packets
+pub trait OutgoingPacket {
+
+    /// Gets this Packet's ID
+    ///
+    /// One day, this will be replaced with an associated constant
+    fn get_id(&self) -> u32;
+
+    /// Encodes this Packet's data
+    fn encode_data(&self, buffer: &mut DequeBuffer);
+
+    /// Encodes this OutgoingPacket
+    fn encode(&self, buffer: &mut DequeBuffer) {
+        
+        buffer.write_unsigned_varint_32(self.get_id());
+        
+        self.encode_data(buffer);
+        
+    }
+    
+    /// Sends this OutgoingPacket to the client
+    fn send(&self, connection: &mut GameConnection) {
+    
+        let mut data_buffer = DequeBuffer::new();
+        
+        self.encode(&mut data_buffer);
+        
+        let mut length_buffer = DequeBuffer::new();
+        
+        length_buffer.write_unsigned_varint_32(data_buffer.remaining() as u32); //TODO: Try to get this as data_buffer.prepend_varint_32 - One day
+        
+        let buffer: Vec<u8> = length_buffer.data.into_iter().chain(data_buffer.data.into_iter()).collect();
+        
+        let _buffer: &[u8] = &buffer[..];
+        
+        if connection.stream.write(_buffer).is_ok() {
+            let _ = connection.stream.flush();
+        }
+    
+    }
+
+}
 
 /*
 use super::super::super::deque_buffer::DequeBuffer;
@@ -17,12 +63,18 @@ pub struct Packet {
 
 }
 
-impl Packet {
+impl OutgoingPacket for Packet {
 
-    /// Gets this Packet's ID
-    pub fn get_id(&self) -> u32 {
     
+    /// Gets this Packet's ID
+    ///
+    /// One day, this will be replaced with an associated constant
+    fn get_id(&self) -> u32 {
         ID_STC_
+    }
+
+    /// Encodes this Packet's data
+    fn encode_data(&self, buffer: &mut DequeBuffer) {
     
     }
 
@@ -30,29 +82,6 @@ impl Packet {
     pub fn new() -> Packet {
     
         Packet { /*etc*/ }
-        
-    }
-    
-    /// Encodes and sends this Packet
-    pub fn send(&self, connection: &mut GameConnection) {
-        
-        let mut data_buffer = DequeBuffer::new();
-        
-        data_buffer.write_unsigned_varint_32(self.get_id());
-        
-        //Write your data here
-        
-        let mut length_buffer = DequeBuffer::new();
-        
-        length_buffer.write_unsigned_varint_32(data_buffer.remaining() as u32); //TODO: Try to get this as write_unsigned_varint_32_front
-        
-        let buffer: Vec<u8> = length_buffer.data.into_iter().chain(data_buffer.data.into_iter()).collect();
-        
-        let _buffer: &[u8] = &buffer[..];
-        
-        if connection.stream.write(_buffer).is_ok() {
-            let _ = connection.stream.flush();
-        }
         
     }
 
