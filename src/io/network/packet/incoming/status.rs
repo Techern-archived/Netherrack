440afa0f8edd33fa723babcb0e23f8ea65cfa5c3
Cruz Julian Bishop <cruzjbishop@gmail.com>
2015-09-01 09:36:29 +1000

    Shorter notation for sending the ListPongPacket

--- src/io/network/packet/incoming/status.rs
+++ src/io/network/packet/incoming/status.rs
@@ -6,7 +6,7 @@ use super::super::super::game_connection::GameConnection; //It's super super sup
 use super::super::PacketHeader;
 
 use super::super::outgoing::OutgoingPacket;
-use super::super::outgoing::status::StatusResponsePacket;
+use super::super::outgoing::status::{StatusResponsePacket, ListPongPacket};
 
 /// A packet sent from the client to make the server send a status response packet
 pub struct StatusRequestPacket {
@@ -25,13 +25,13 @@ impl StatusRequestPacket {
     /// Actual functionality will be handled in the handle function
     #[allow(unused_variables)]
     pub fn decode(header: PacketHeader, connection: &mut GameConnection, buffer: &mut DequeBuffer) -> StatusRequestPacket {
-                
+
         // Nothing to do here
-        
+
         StatusRequestPacket { header: header /*etc*/ }
-        
+
     }
-    
+
     /// Handles this StatusRequestPacket
     pub fn handle(&self, connection: &mut GameConnection) {
         StatusResponsePacket::new().send(connection);
@@ -44,7 +44,7 @@ pub struct ListPingPacket {
 
     /// The header of this ListPingPacket
     pub header: PacketHeader,
-    
+
     /// The ping ID (usually the system time in milliseconds)
     pub payload: i64
 
@@ -59,16 +59,16 @@ impl ListPingPacket {
     /// Actual functionality will be handled in the handle function
     #[allow(unused_variables)]
     pub fn decode(header: PacketHeader, connection: &mut GameConnection, buffer: &mut DequeBuffer) -> ListPingPacket {
-                
+
         let id: i64 = buffer.read_signed_long();
-        
+
         ListPingPacket { header: header, payload: id }
-        
+
     }
-    
+
     /// Handles this ListPingPacket
     pub fn handle(&self, connection: &mut GameConnection) {
-        super::super::outgoing::status::ListPongPacket::new(self.payload).send(connection);
+        ListPongPacket::new(self.payload).send(connection);
     }
 
 }
