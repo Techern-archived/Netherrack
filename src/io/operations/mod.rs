90b4f4ece65a0f45e8448fb636536a63bc9ca0b6
Cruz Julian Bishop <cruzjbishop@gmail.com>
2015-09-02 15:58:50 +1000

    Test native use of Varint32 zero

--- src/io/operations/mod.rs
+++ src/io/operations/mod.rs
@@ -18,12 +18,14 @@ mod test {
         let mut vector = Cursor::new(vec![0u8; 0]);
 
         assert!(vector.write_unsigned_varint_32(15).is_ok());
+        assert!(vector.write_unsigned_varint_32(0).is_ok());
         assert!(vector.write_unsigned_varint_32(2111111111).is_ok());
         assert!(vector.write_unsigned_varint_32(3463465).is_ok());
 
         vector.set_position(0);
 
         assert_eq!(15, vector.read_unsigned_varint_32().unwrap());
+        assert_eq!(0, vector.read_unsigned_varint_32().unwrap());
         assert_eq!(2111111111, vector.read_unsigned_varint_32().unwrap());
         assert_eq!(3463465, vector.read_unsigned_varint_32().unwrap());
 
